e672c76bbabfbbd32fb6545cfc3c11421375f26f
fahim-ahmed-7861 <fahimahmedshojib109@gmai.com>
2021-04-22 16:30:38 +0600

    'done'

--- Login_app/views.py
+++ Login_app/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
-from Login_app.forms import UserInfoForm,UserForm
+from Login_app.forms import UserForm, UserInfoForm
+from Login_app.models import UserInfo
+from django.contrib.auth.models import User
 
-
-from django.contrib.auth import authenticate,login,logout
-from  django.http import  HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-
 
 
 # Create your views here.
 
 def index(request):
-    diction = {}
-    return render(request, 'Login_app/index.html', context=diction)
+    dict={}
+    if request.user.is_authenticated:
+        current_user = request.user
+        user_id = current_user.id
+        user_basic_info = User.objects.get(pk=user_id)
+        user_more_info = UserInfo.objects.get(user__pk=user_id)
+        dict = {'user_basic_info':user_basic_info, 'user_more_info':user_more_info}
+    return render(request, 'Login_app/index.html', context=dict)
 
 def login_page(request):
     return render(request,'Login_app/login.html',context={})
@@ -40,39 +46,41 @@ def user_login(request):
         return HttpResponseRedirect(reverse('Login_app:login'))
 
 
+@login_required
+
+def user_logout(request):
+    
+    logout(request)
+    
+    return HttpResponseRedirect(reverse('Login_app:index'))
+    
+
+
 
 
 def register(request):
-
-    registered=False
-
+    registered = False
 
     if request.method == 'POST':
-        user_form= UserForm(data=request.POST)
-        user_info_form = UserInfoForm(data= request.POST)
+        user_form = UserForm(data=request.POST)
+        user_info_form = UserInfoForm(data=request.POST)
 
         if user_form.is_valid() and user_info_form.is_valid():
-            user =user_form.save()
+            user = user_form.save()
             user.set_password(user.password)
-
             user.save()
 
             user_info = user_info_form.save(commit=False)
-
-            user_info.user=user
+            user_info.user = user
 
             if 'profile_pic' in request.FILES:
-                user_info.profile_pics=request.FILES['profile_pics']
+                user_info.profile_pic = request.FILES['profile_pic']
 
             user_info.save()
-
-            registered=True
-
-
+            registered = True
     else:
-     user_form = UserForm()
-     user_info_form = UserInfoForm()
+        user_form = UserForm()
+        user_info_form = UserInfoForm()
 
-    dict = {'user_form': user_form,'user_info_form' : user_info_form,
-            'registered' : registered}
-    return render(request,"Login_app/register.html",context=dict)
+    dict = {'user_form': user_form, 'user_info_form': user_info_form, 'registered': registered}
+    return render(request, 'Login_app/register.html', context=dict)
--- My_Second_Porject/settings.py
+++ My_Second_Porject/settings.py
@@ -133,6 +133,10 @@ STATIC_DIR,
 MEDIA_ROOT=MEDIA_DIR
 MEDIA_URL='/media/'
 
+#Login
+
+LOGIN_URL='/login/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
